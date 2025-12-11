b18b707f5bfbbbfaf3a972acda6ca3dd2acdb2d9
Pavankalyan Naragani <pavankalyannaragani@gmail.com>
2025-12-11 15:38:57 +0530

    push

--- backend/chatbot_main/deployment_settings.py
+++ backend/chatbot_main/deployment_settings.py
@@ -3,10 +3,10 @@ import dj_database_url
 from .settings import * 
 from .settings import BASE_DIR
 
-ALLOWED_HOSTS = [os.environ.get('RENDER_EXTERNAL_HOSTNAME')]
+ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = ['https://'+os.environ.get('RENDER_EXTERNAL_HOSTNAME')]
 
-DEBUG = False
+DEBUG = True
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 MIDDLEWARE = [
